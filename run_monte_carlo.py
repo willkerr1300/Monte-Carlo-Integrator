69d1ccfb912618a4917a72bc53e7bdcf24a81171
willkerr1300 <williamkerr1300@gmail.com>
2025-10-15 18:59:58 -0400

    Update run_monte_carlo.py

--- run_monte_carlo.py
+++ run_monte_carlo.py
@@ -1,8 +1,13 @@
-from MC_Integrator import monte_carlo_integrate
+import time
+from monte_carlo_wrapper import monte_carlo_integrate
 
 dim = 5
 n_samples = 10_000_000
+batch_size = 1_000_000
 
-result = monte_carlo_integrate(dim, n_samples, batch_size=1_000_000)
+start = time.perf_counter()
+result = monte_carlo_integrate(dim, n_samples, batch_size)
+end = time.perf_counter()
 
-print("Monte Carlo estimate:", result)
+print(f"Monte Carlo estimate: {result}")
+print(f"Time elapsed: {end - start:.3f} seconds")
