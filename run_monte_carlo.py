9caceee9a39b2272a022352b1e3badfb226c0964
willkerr1300 <williamkerr1300@gmail.com>
2025-10-15 18:36:54 -0400

    Create run_monte_carlo.py

---
+++ run_monte_carlo.py
@@ -0,0 +1,8 @@
+from MC-Integrator import monte_carlo_integrate
+
+dim = 5
+n_samples = 10_000_000
+
+result = monte_carlo_integrate(dim, n_samples, batch_size=1_000_000)
+
+print("Monte Carlo estimate:", result)
